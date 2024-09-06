1abefd297ed09403d3f194b9f120025a52faaaa5
Vũ Văn Thanh <thanhvv@Vus-MacBook-Pro.local>
2024-09-07 00:07:15 +0700

    handle response

---
+++ src/core/success.response.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_ERROR: 500,
+};
+
+const ReasonStatusCode = {
+  OK: "Success",
+  CREATED: "Created",
+  BAD_REQUEST: "Bad request",
+  UNAUTHORIZED: "Unauthorized",
+  FORBIDDEN: "Forbidden",
+  NOT_FOUND: "Not found",
+  INTERNAL_ERROR: "Internal error",
+};
+
+class SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCode.OK,
+    reasonStatusCode = ReasonStatusCode.OK,
+    metadata = {},
+  }) {
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res, headers = {}) {
+    return res.status(this.status).json(this);
+  }
+}
+
+class OK extends SuccessResponse {
+  constructor({ message, metadata }) {
+    super({ message, metadata });
+  }
+}
+
+class CREATED extends SuccessResponse {
+  constructor({
+    message,
+    statusCode = StatusCode.CREATED,
+    reasonStatusCode = ReasonStatusCode.CREATED,
+    metadata,
+    options = {},
+  }) {
+    super({ message, statusCode, reasonStatusCode, metadata });
+    this.options = options;
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED,
+};
